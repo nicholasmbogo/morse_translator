36047b5672892e820e38fa235796598c1140d95a
NicholasMbogo <nicholasmbogo@yahoo.com>
2017-10-11 11:02:16 -0600

    updated morse_traslator

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,2 +1,60 @@
+require 'pry'
 class Translator
+
+
+  def dictionary
+                  {"a" => ".-",
+                	"b" => "-...",
+                	"c" => "-.-.",
+                	"d" => "-..",
+                	"e" => ".",
+                	"f" => "..-.",
+                	"g" => "--.",
+                	"h" => "....",
+                	"i" => "..",
+                	"j" => ".---",
+                	"k" => "-.-",
+                	"l" => ".-..",
+                	"m" => "--",
+                	"n" => "-.",
+                	"o" => "---",
+                	"p" => ".--.",
+                	"q" => "--.-",
+                	"r" => ".-.",
+                	"s" => "...",
+                	"t" => "-",
+                	"u" => "..-",
+                	"v" => "...-",
+                	"w" => ".--",
+                	"x" => "-..-",
+                	"y" => "-.--",
+                	"z" => "--..",
+                	" " => " ",
+                	"1" => ".----",
+                	"2" => "..---",
+                	"3" => "...--",
+                	"4" => "....-",
+                	"5" => ".....",
+                	"6" => "-....",
+                	"7" => "--...",
+                	"8" => "---..",
+                	"9" => "----.",
+                	"0" => "-----"
+                }
+
+
+  end
+
+  def eng_to_morse(text)
+    #dictionary[text]
+
+    translated = text.downcase.chars.map do |char|
+      dictionary[char]
+    end.join
+  end
+
+  def from_file(filename)
+    file = File.read(filename)
+    eng_to_morse(text)
+  end
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -9,5 +10,41 @@ def test_translator_exists
     translator = Translator.new
 
     assert_instance_of Translator, translator
+  end
+
+  def test_can_translate_a_single_letter
+    translator = Translator.new
+
+    assert_equal translator.eng_to_morse("h"), "...."
+  end
+
+  def test_can_translate_two_letters
+    translator = Translator.new
+
+    assert_equal translator.eng_to_morse("he"), "....."
+  end
+
+  def test_can_translate_a_string_of_words
+    translator = Translator.new
+
+    assert_equal translator.eng_to_morse("hello world"), "......-...-..--- .-----.-..-..-.."
+  end
+
+  def test_can_translate_case_incensitive_characters
+    translator = Translator.new
+
+    assert_equal translator.eng_to_morse("Hello World"), "......-...-..--- .-----.-..-..-.."
+  end
+
+  def test_can_translate_a_text_with_numbers
+    translator = Translator.new
+
+    assert_equal translator.eng_to_morse("There are 3 ships"),  "-......-.. .-.-.. ...-- ..........--...."
+  end
+
+  def test_can_read_and_translate_from_a_file
+    translator = Translator.new
+    
+    assert_equal ".. .--- ..-. .- ..-....-...", translator.from_file("input.txt")
   end
 end
